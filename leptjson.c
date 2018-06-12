11ae9a2e113fcd254fb0b88c059e8f4a55062322
kevin <kevin@kr3d.com>
2018-06-12 15:37:28 +0800

    number

--- leptjson.c
+++ leptjson.c
@@ -1,10 +1,14 @@
-#include "leptjson.h"
+﻿#include "leptjson.h"
 
 #include <assert.h>  /* assert() */
-#include <stdlib.h>  /* NULL */
-#include <stdio.h>
+#include <errno.h>   /* errno, ERANGE */
+#include <math.h>    /* HUGE_VAL */
+#include <stdlib.h>  /* NULL, strtod() */
 
 #define EXPECT(c, ch) do { assert(*c->json == (ch)); c->json++; } while(0)
+
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 
 typedef struct
 {
@@ -39,14 +43,30 @@ static int lept_parse_literal(lept_context* c, lept_value* v, const char* litera
 
 static int lept_parse_number(lept_context* c, lept_value* v)
 {
-	char* end;
-	/* \TODO validate number */
-	v->n = strtod(c->json, &end);
-	if (c->json == end)
-		return LEPT_PARSE_INVALID_VALUE;
-
-	c->json = end;
+	const char* p = c->json;
+	if (*p == '-') p++;
+	if (*p == '0') p++;
+	else {
+		if (!ISDIGIT1TO9(*p)) return LEPT_PARSE_INVALID_VALUE;
+		for (p++; ISDIGIT(*p); p++);
+	}
+	if (*p == '.') {
+		p++;
+		if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+		for (p++; ISDIGIT(*p); p++);
+	}
+	if (*p == 'e' || *p == 'E') {
+		p++;
+		if (*p == '+' || *p == '-') p++;
+		if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+		for (p++; ISDIGIT(*p); p++);
+	}
+	errno = 0;
+	v->n = strtod(c->json, NULL);
+	if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+		return LEPT_PARSE_NUMBER_TOO_BIG;
 	v->type = LEPT_NUMBER;
+	c->json = p;
 	return LEPT_PARSE_OK;
 }
 
@@ -80,8 +100,11 @@ int lept_parse(lept_value* v, const char* json)
 	if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK)
 	{
 		lept_parse_whitespace(&c);
-		if (*c.json != '\0')
+		if (*c.json != '\0') 
+		{
+			v->type = LEPT_NULL;
 			ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+		}
 	}
 
 	return ret;
